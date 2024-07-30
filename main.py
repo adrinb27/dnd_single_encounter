574793c4614679775be88ff3bf69fe9717ecbc30
adrinb27 <adrinb@microsoft.com>
2024-07-30 17:07:46 -0600

    updated changes

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 from clients import AzureOpenAIClient, AzureCosmosDBClient
 from session import Session , World , Location , Encounter
 load_dotenv()
+app_version = "0.2.0"
 
 
 async def main():
@@ -28,8 +29,16 @@ async def main():
     encounter = Encounter()
     
     # TODO create session leger document, add app version 
-    item_session = {"id":session.session_id,"sessionid_type":f"{session.session_id}_{session.type}","session_id":session.session_id,"type":session.type,"status":"Creating","update_reason":session.type}
-    azure_cosmos_client.insert_items(item_session)
+    item = {
+           "id":session.session_id,
+           "sessionid_type":f"{session.session_id}_{session.type}",
+           "session_id":session.session_id,
+           "type":session.type,
+           "app_version":app_version,
+           "status":"Creating",
+           "update_reason":session.type
+           }
+    azure_cosmos_client.insert_items(item)
 
     #create world
     await world.create_world(
@@ -39,9 +48,14 @@ async def main():
                 )
     
     # insert world item to cosmos container
-    item_world = {"id":session.session_id,"session_id":session.session_id,"type":world.type,"sessionid_type":f"{session.session_id}_{world.type}",world.type:world.world}  
-    azure_cosmos_client.insert_items(item_world)
-    world.item = item_world
+    item = { 
+           "id":session.session_id,
+           "session_id":session.session_id,
+           "type":world.type,"sessionid_type":f"{session.session_id}_{world.type}",
+           world.type:world.world
+           }  
+    azure_cosmos_client.insert_items(item)
+    world.item = item
 
     # Update leger
     operations = [{ 'op': 'replace', 'path': '/update_reason', 'value': world.type },
@@ -61,7 +75,13 @@ async def main():
                 system_prompt=prompt_json['location_json'][0],
                 prompt=user_text
     )
-    item = {"id":session.session_id,"session_id":session.session_id,"type":location.type,"sessionid_type":f"{session.session_id}_{location.type}",location.type:location.location}  
+    item = {
+           "id":session.session_id,
+           "sessionid_type":f"{session.session_id}_{location.type}",
+           "session_id":session.session_id,
+           "type":location.type,
+           location.type:location.location
+           }  
     azure_cosmos_client.insert_items(item)
     location.item = item
 
@@ -76,25 +96,45 @@ async def main():
     # # create 4 characters and save it
     # # loop and save the type to create unique characters
 
-    # # create encounter
-    # location_description = location.location["description"]
+    # create encounter
+    location_description = location.location["description"]
     
-    # user_text = f"Create an Encounter using this description: {location_description}"
+    user_text = f"Create an Encounter using this description: {location_description}"
     
-    # await encounter.create_encounter(
-    #             client=azure_open_ai_client,
-    #             system_prompt=prompt_json['encounter_json'][0],
-    #             prompt=user_text
-    # )
-    # item = {"id":session.session_id,"session_id":session.session_id,"type":encounter.type,"sessionid_type":f"{session.session_id}_{encounter.type}",encounter.type:encounter.encounter}
-    # # print(item["encounter"])
-    # azure_cosmos_client.insert_items(item)
-    # encounter.item = item
+    await encounter.create_encounter(
+                client=azure_open_ai_client,
+                system_prompt=prompt_json['encounter_json'][0],
+                prompt=user_text
+    )
+    item = {
+           "id":session.session_id,
+           "sessionid_type":f"{session.session_id}_{encounter.type}",
+           "session_id":session.session_id,
+           "type":encounter.type,
+           encounter.type:encounter.encounter
+           }
+    # print(item["encounter"])
+    azure_cosmos_client.insert_items(item)
+    encounter.item = item
+
+    # Update leger
+    operations = [{ 'op': 'replace', 'path': '/update_reason', 'value': encounter.type },
+            { 'op': 'add', 'path': f'/{encounter.type}_name', 'value': encounter.encounter["name"]}
+                 ]
+    
+    azure_cosmos_client.patching_items(session.session_id,f"{session.session_id}_{session.type}",operations)
 
     # # TODO create creature
 
     # # Finish Session:
-    # item = {"id":session.session_id,"session_id":session.session_id,"type":"session","status":"Done","title":""}
+    # Update leger
+    operations = [
+           { 'op': 'replace', 'path': '/status', 'value': "Done" }
+                 ]
+    
+    azure_cosmos_client.patching_items(session.session_id,f"{session.session_id}_{session.type}",operations)
+
+    # TODO Creatures on the encounter
 
 
 
